52af929e87e76958dbeca9de532ac98ee66c5e9f
Gokula krishnan R <towner.mvpproject@gmail.com>
2024-08-27 22:18:58 +0530

    chore: Update header images with new URLs

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
             <div className="flex items-center space-x-4">
               <div className="flex items-center gap-2">
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/4ff0/c88d/1a68ec349c9d92d9c4bdffcba943d276?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eNlOzfRmzh7mcsTOdzEfGT77gqv73YT7gjpTKt5-vmLwHF40O7FfS10RtoDgwrB5X~lyImvL7Rbk2h6Djyc6GplUseRwKG2jOqh~qWkzyd3Eb~MfVQR--COoSp9GL9G-g4c-uV71fwSDbhfTJmvIvrdy-Bw5GLzNhmLoMPS-XDOSHpWkmt3zi~itCNGplM6gDs5siTyP6sOOR0OycPHiUVOAIS9oxamzdxtXWVTXqqVNfxuJwLLfo7gs-PuoTA8EtTR7m-LIWY-9S4hxCpU~nMZCfkrtkv95zXvutS~7tkfWqf1zbQmEd684fTPByJgWntgbIKFVURxIyvrvGgEQWw__"
+                  src="https://s3-alpha-sig.figma.com/img/4ff0/c88d/1a68ec349c9d92d9c4bdffcba943d276?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YoL6jt2W6IvXIZOgnPb8YcxrBm0Cf1VVBMRI3fOuD2W8jm37URuuW8faIw4vb4U2yqWJrwmjRIpKHSDZvph4aVQ07y5EykrI7g48jx5RhQ3QVAKqvKVk3dgEtwnrs7~OdnJZ1jJvopDvFz9N7eeCUYmY1AidDzhaSDEtsXp0MRt5Yw6uWGMHUa9u-ktKWMHamZ6AWo73HgOj5MrNvgf2IsuzTRDSkds9CYGCAZGzI5q1Q32zhOgzD6Tdh22x~3bXpDTXZp7jO~XW5p5rbklDXKGx-Q96mj4rYIPuSzof4IxeKko4~f33F6jcjtMMlM3QtN-xRoDQoTlqLSJPSO6fqA__"
                   alt=""
                   className="w-8 h-8"
                 />
@@ -22,7 +22,7 @@ const Header = () => {
               </div>
               <div className="flex items-center gap-2">
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/a791/f741/66ad2e238edb2e879f009fd87a220085?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CtC0zeKq1DedDykoy1iq~KKyRCcNCxOnfOiYv0lD7TBegLH8lW7Eu6cRvytzDs4TLHPvHPbr7MGcNvRLdCf3SGvewTrFXAh9Z0nZl2iT~5iwKY50MxafzswbMjK7dzHXd-XZcrLdHbP7mD-2tcvnn3Mbo8JQaZW9BmNoT4EEYlFGlxF63edwZaRwiKICEzihUV3IEz5eyw6tZm5MxUFFFhLu-jAMAfZO3L71HyAY-pyUisIHyxx~kP5YcNzzOdkJcYpIddzG0VXVCjPPplVvYGHuGP-Qtq5uHztLqHLjHx1Ej8VK5TxkttmxwPyZEHemqpeHlW2Pt49C3GDCOIkgdg__"
+                  src="https://s3-alpha-sig.figma.com/img/a791/f741/66ad2e238edb2e879f009fd87a220085?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nSfdSQa8cxT~yzcus54wh74l7CB2ItQSOzjU56~Yg48AZj6iEwB9IP0wogar6qFj4oQnUGsoUI2NPuTGffFmcZO4B540wBxYSd~KPI3fP84hf~4Vdnr9RQdVXsCOpgWbaY94lzmf8r9HIXCQqqzfQ1e~DN~YhNWK8Zqk4xns5hKO1fZukD1PeEmrIMs7s6YUcT5RAsBGx1RmIohmPpQXdIlwEMvUKpENnqw8jlpW5zHmwyFnXZU5~Uj4RAU5cDwt56uvtZe19Z55kR11b8aEiBUQECrlENvAOcFd-bD1mqR4OnGTDXVOFmu2MQG4MY4rM4NE600BxAkgRcm1A9LrLg__"
                   alt=""
                   className="w-8 h-8"
                 />
