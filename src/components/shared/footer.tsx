ba7778e95fddf9971f5bb6018892e40f9756a212
Gokula krishnan R <towner.mvpproject@gmail.com>
2024-10-08 16:30:56 +0530

    Refactor footer component to fix broken link in About Us page

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                   <Link
-                    href="/aboutus"
+                    href="/about-us"
                     className="hover:underline me-4 md:me-6"
                   >
                     About Us
