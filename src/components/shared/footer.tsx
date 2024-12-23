3740b202b62bc22a35937859689e89902c6c1c98
Shadowctrl <shadowctrl@outlook.com>
2024-12-23 16:09:23 +0530

    Mobile Align - Home page before Login

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -5,57 +5,63 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="mt-12 pt-2 bg-slate-50/50">
-      <div className="w-full mx-auto p-0 md:py-0">
-        <div className="sm:flex sm:items-center sm:justify-between px-10">
-          <div className="flex justify-between flex-col">
+      <div className="w-full mx-auto">
+        <div className="flex flex-col md:flex-row md:items-start md:justify-between px-4 md:px-10 space-y-6 md:space-y-0">
+          <div className="flex flex-col">
             <Link
               href="/"
-              className="flex items-center sm:mb-0 space-x-3 rtl:space-x-reverse"
+              className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               <Image
-                src={"/logo.png"}
+                src="/logo.png"
                 alt="TenderOnline"
                 width={280}
                 height={95}
+                className="w-48 md:w-72"
               />
             </Link>
-            <span>The People. The Government. The Business</span>
+            <span className="text-sm md:text-base text-gray-600">
+              The People. The Government. The Business
+            </span>
           </div>
-          <div className="">
-            <div className="text-right flex-col">
+
+          <div className="flex flex-col space-y-4">
+            <div className="flex flex-col items-start md:items-end">
               <p className="text-[14px] font-bold uppercase text-[#4A4A4A]">
                 Find us Online
               </p>
-              <div className="flex gap-2 justify-end mt-1">
-                <Link href={"https://linkedin.com/in"} target="_blank">
+              <div className="flex gap-4 md:gap-2 mt-1">
+                <Link href="https://linkedin.com/in" target="_blank">
                   <Image
                     src="/linkedin1.png"
-                    width="26"
+                    width={26}
                     height={29}
                     alt="Linkedin Handle"
+                    className="w-6 h-6"
                   />
                 </Link>
-                <Link href={"https://x.com/in"} target="_blank">
-                  {" "}
+                <Link href="https://x.com/in" target="_blank">
                   <Image
                     src="/twitter1.png"
-                    width="26"
+                    width={26}
                     height={29}
                     alt="X Handle"
+                    className="w-6 h-6"
                   />
                 </Link>
-                <Link href={"https://facebook.com/in"} target="_blank">
-                  {" "}
+                <Link href="https://facebook.com/in" target="_blank">
                   <Image
                     src="/facebook1.png"
-                    width="26"
+                    width={26}
                     height={29}
                     alt="Facebook Handle"
+                    className="w-6 h-6"
                   />
                 </Link>
               </div>
             </div>
-            <ul className="flex flex-wrap gap-4 items-center mb-6 text-[16px] sm:mb-0 mt-4">
+
+            <ul className="flex flex-wrap gap-4 items-center text-sm md:text-base">
               <li>
                 <Link href="/about-us" className="hover:underline">
                   About Us
@@ -76,44 +82,47 @@ const Footer = () => {
                   Blog
                 </Link>
               </li>
-            </ul>{" "}
+            </ul>
           </div>
         </div>
-        <div className="my-6 sm:mx-auto lg:my-4" />
-        <div className="flex items-center justify-between bg-[#202020] h-[55px] px-10">
-          <div className="">
-            <span className="block text-[16px] text-[#D9D9D9] sm:text-center">
-              © 2024{" "}
-              <a href="/" className="hover:underline">
-                TenderOnline{" "}
-              </a>
-            </span>
-          </div>
-          <div className="">
-            <ul className="flex text-[14px] text-white items-center gap-5">
-              <li>
-                <Link href="/terms" className="hover:underline">
-                  Terms & Conditions
-                </Link>
-              </li>
-              <li>
-                <Link href="/privacy-policy" className="hover:underline">
-                  Privacy Policy
-                </Link>
-              </li>
 
-              <li>
-                <Link href="/cookie" className="hover:underline">
-                  Cookie Policy
-                </Link>
-              </li>
+        <div className="bg-[#202020] mt-4">
+          <div className="px-4 md:px-10 py-4">
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
+              <div className="text-center md:text-left">
+                <span className="text-sm md:text-base text-[#D9D9D9]">
+                  © 2024{" "}
+                  <Link href="/" className="hover:underline">
+                    TenderOnline
+                  </Link>
+                </span>
+              </div>
 
-              <li>
-                <Link href="/refund-policy" className="hover:underline">
-                  Refund Policy
-                </Link>
-              </li>
-            </ul>
+              <div className="w-full md:w-auto">
+                <ul className="flex flex-wrap justify-center md:justify-end text-xs md:text-sm text-white items-center gap-4 md:gap-5">
+                  <li>
+                    <Link href="/terms" className="hover:underline">
+                      Terms & Conditions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/privacy-policy" className="hover:underline">
+                      Privacy Policy
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/cookie" className="hover:underline">
+                      Cookie Policy
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/refund-policy" className="hover:underline">
+                      Refund Policy
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -17,8 +17,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import Signup from "@/app/(auth)/signup/page";
-import Image from "next/image";
 
 export function DropdownMenuDemo() {
   const router = useRouter();
@@ -102,13 +100,10 @@ export function DropdownMenuDemo() {
   );
 }
 
-interface HeaderProps {
-  isLogin1?: boolean;
-  setIsLogin1?: (value: boolean) => void;
-}
-
-const Header = ({ isLogin1, setIsLogin1 }: HeaderProps) => {
+const Header = ({ isLogin1, setIsLogin1 }: any) => {
   const [isLogin, setIsLogin] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     const token = sessionStorage.getItem("accessToken");
     if (token) {
@@ -119,156 +114,198 @@ const Header = ({ isLogin1, setIsLogin1 }: HeaderProps) => {
   }, []);
 
   return (
-    <div>
-      <div className="absolute z-40 top-8 right-0 px-2 lg:px-24 left-0">
-        <div className="border bg-white rounded-xl px-6 py-2 w-full shadow-lg">
-          <div className="flex justify-between">
-            <div className="text-2xl flex items-center gap-2 font-bold">
-              <Link href={"/"}>
-                <img src="/logo.png" className="h-8 lg:h-16" alt="" />
-              </Link>
-              <hr className="border-t rotate-90 h-6 w-10" />
-              <div className="flex items-center gap-6 pl-6">
-                <Link
-                  className="text-[16px] font-sans font-normal text-[#4B4B4B]"
-                  href={"/"}
-                >
-                  Home
+    <div className="relative">
+      <div className="absolute z-40 top-0 right-0 left-0 px-2 md:px-4 lg:px-24 pt-4">
+        <div className="border bg-white rounded-xl px-2 md:px-6 py-2 w-full shadow-lg">
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="flex items-center justify-between md:justify-start">
+              <div className="flex items-center gap-2">
+                <Link href="/">
+                  <img src="/logo.png" className="h-8 lg:h-12" alt="Logo" />
                 </Link>
-                <Link
-                  className="text-[16px] font-sans font-normal text-[#4B4B4B]"
-                  href={"/about-us"}
-                >
-                  About Us
-                </Link>
-                <Link
-                  className="text-[16px] font-sans font-normal text-[#121212]"
-                  href={"/pricing"}
-                >
-                  Pricing
-                </Link>
-                <Link
-                  className="text-[16px] font-sans font-normal text-[#4B4B4B]"
-                  href={"/blog"}
-                >
-                  Blog
-                </Link>
+                <hr className="border-t rotate-90 h-6 w-6 md:w-10 hidden md:block" />
               </div>
-            </div>
-            <div className="flex items-center space-x-6">
-              {isLogin && (
-                <div className="hidden lg:block">
-                  <Link
-                    href={"/tenders"}
-                    className="border text-center text-sm font-semibold text-[#0c1073] border-[#0c1073] rounded-xl px-6 w-full mx-4 py-2"
-                  >
-                    Tenders Dashboard
-                  </Link>
-                </div>
-              )}
 
-              {/* <Link
-                target="_blank"
-                href="mailto:sales@tenderonline.co.in"
-                className="flex items-center gap-2"
+              <button
+                className="md:hidden p-2"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
               >
-                <img src="/gmail.png" alt="Gmail" className="w-8 h-8" />
-                <h1 className="font-bold hidden lg:block text-sm">
-                  sales@tenderonline.co.in
-                </h1>
-              </Link> */}
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d={
+                      isMenuOpen
+                        ? "M6 18L18 6M6 6l12 12"
+                        : "M4 6h16M4 12h16M4 18h16"
+                    }
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <div
+              className={`${
+                isMenuOpen ? "flex" : "hidden"
+              } md:hidden flex-col space-y-4 py-4`}
+            >
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/"
+              >
+                Home
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/about-us"
+              >
+                About Us
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/pricing"
+              >
+                Pricing
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/blog"
+              >
+                Blog
+              </Link>
+
+              {isLogin && (
+                <Link
+                  href="/tenders"
+                  className="border text-center text-sm font-semibold text-[#0c1073] border-[#0c1073] rounded-xl px-4 py-2"
+                >
+                  Tenders Dashboard
+                </Link>
+              )}
 
               {!isLogin && (
-                <div className="hidden lg:block">
-                  <div>
-                    {isLogin1 && (
-                      <div className="flex gap-2">
-                        <div className="px-4 py-2 text-sm font-semibold rounded-xl bg-black text-white">
-                          <Dialog>
-                            <DialogTrigger>Watch Demo</DialogTrigger>
-                            <DialogContent className="sm:max-w-[903px] bg-white">
-                              <DialogHeader>
-                                <DialogTitle>Watch Demo</DialogTitle>
-                                <DialogDescription>
-                                  Get 3 Days Free Trial. No payment required
-                                </DialogDescription>
-                              </DialogHeader>
-                              <iframe
-                                width="853"
-                                height="480"
-                                src={`https://www.youtube.com/embed/rokGy0huYEA`}
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                title="Embedded youtube"
-                                className="rounded-md"
-                              />
-                            </DialogContent>
-                          </Dialog>
-                        </div>
-                        <button
-                          className="rounded-xl px-4 py-2 bg-black text-white font-semibold text-[16px]"
-                          onClick={() => {
-                            console.log(isLogin1, setIsLogin1);
-
-                            if (window.location.pathname !== "/") {
-                              window.location.href = "/";
-                            } else if (setIsLogin1 !== undefined) {
-                              setIsLogin1(false);
-                            }
-                          }}
-                        >
-                          Sign Up
-                        </button>
-                      </div>
-                    )}
-                    {!isLogin1 && (
-                      <div className="flex gap-2">
-                        <div className="px-4 py-2 text-sm font-semibold rounded-xl bg-black text-white">
-                          <Dialog>
-                            <DialogTrigger>Watch Demo</DialogTrigger>
-                            <DialogContent className="sm:max-w-[903px] bg-white">
-                              <DialogHeader>
-                                <DialogTitle>Watch Demo</DialogTitle>
-                                <DialogDescription>
-                                  Get 3 Days Free Trial. No payment required
-                                </DialogDescription>
-                              </DialogHeader>
-                              <iframe
-                                width="853"
-                                height="480"
-                                src={`https://www.youtube.com/embed/rokGy0huYEA`}
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                title="Embedded youtube"
-                                className="rounded-md"
-                              />
-                            </DialogContent>
-                          </Dialog>
-                        </div>
-                        <button
-                          className="rounded-xl px-4 py-2 bg-black text-white font-semibold text-[16px]"
-                          onClick={() => {
-                            console.log(isLogin1, setIsLogin1);
-
-                            if (window.location.pathname !== "/") {
-                              window.location.href = "/";
-                            } else if (setIsLogin1 !== undefined) {
-                              setIsLogin1(true);
-                            }
-                          }}
-                        >
-                          Sign In
-                        </button>
-                      </div>
-                    )}
-                  </div>
+                <div className="flex flex-col space-y-2">
+                  <Dialog>
+                    <DialogTrigger className="px-4 py-2 text-sm font-semibold rounded-xl bg-black text-white w-full">
+                      Watch Demo
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[903px] bg-white">
+                      <DialogHeader>
+                        <DialogTitle>Watch Demo</DialogTitle>
+                        <DialogDescription>
+                          Get 3 Days Free Trial. No payment required
+                        </DialogDescription>
+                      </DialogHeader>
+                      <iframe
+                        width="100%"
+                        height="480"
+                        src="https://www.youtube.com/embed/rokGy0huYEA"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube"
+                        className="rounded-md"
+                      />
+                    </DialogContent>
+                  </Dialog>
+                  <button
+                    className="rounded-xl px-4 py-2 bg-black text-white font-semibold text-sm w-full"
+                    onClick={() => {
+                      if (window.location.pathname !== "/") {
+                        window.location.href = "/";
+                      } else if (setIsLogin1 !== undefined) {
+                        setIsLogin1(!isLogin1);
+                      }
+                    }}
+                  >
+                    {isLogin1 ? "Sign Up" : "Sign In"}
+                  </button>
                 </div>
               )}
+            </div>
+
+            <div className="hidden md:flex items-center gap-6">
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/"
+              >
+                Home
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/about-us"
+              >
+                About Us
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/pricing"
+              >
+                Pricing
+              </Link>
+              <Link
+                className="text-sm font-sans text-gray-600 hover:text-gray-900"
+                href="/blog"
+              >
+                Blog
+              </Link>
+            </div>
+
+            <div className="hidden md:flex items-center space-x-4">
               {isLogin && (
-                <div className="hidden lg:block">
-                  <DropdownMenuDemo />
+                <Link
+                  href="/tenders"
+                  className="border text-center text-sm font-semibold text-[#0c1073] border-[#0c1073] rounded-xl px-4 py-2"
+                >
+                  Tenders Dashboard
+                </Link>
+              )}
+
+              {!isLogin && (
+                <div className="flex gap-2">
+                  <Dialog>
+                    <DialogTrigger className="px-4 py-2 text-sm font-semibold rounded-xl bg-black text-white">
+                      Watch Demo
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[903px] bg-white">
+                      <DialogHeader>
+                        <DialogTitle>Watch Demo</DialogTitle>
+                        <DialogDescription>
+                          Get 3 Days Free Trial. No payment required
+                        </DialogDescription>
+                      </DialogHeader>
+                      <iframe
+                        width="853"
+                        height="480"
+                        src="https://www.youtube.com/embed/rokGy0huYEA"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube"
+                        className="rounded-md"
+                      />
+                    </DialogContent>
+                  </Dialog>
+                  <button
+                    className="rounded-xl px-4 py-2 bg-black text-white font-semibold text-sm"
+                    onClick={() => {
+                      if (window.location.pathname !== "/") {
+                        window.location.href = "/";
+                      } else if (setIsLogin1 !== undefined) {
+                        setIsLogin1(!isLogin1);
+                      }
+                    }}
+                  >
+                    {isLogin1 ? "Sign Up" : "Sign In"}
+                  </button>
                 </div>
               )}
+
+              {isLogin && <DropdownMenuDemo />}
             </div>
           </div>
         </div>
