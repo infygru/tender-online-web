e6b1a7080faa7cae598ad27d3d70fafc7b56c68e
Shadowctrl <shadowctrl@outlook.com>
2024-12-25 20:22:06 +0530

    `Update YouTube video ID in about-us page`

--- src/app/(default)/about-us/page.tsx
+++ src/app/(default)/about-us/page.tsx
@@ -94,7 +94,7 @@ const Page = () => {
                     <iframe
                       width="853"
                       height="480"
-                      src={`https://www.youtube.com/embed/rokGy0huYEA`}
+                      src={`https://www.youtube.com/embed/1a1LnqGmZVc?si=bv8Ymgm4t4IBLqJC`}
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen
                       title="Embedded youtube"
